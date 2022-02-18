7f4713f74a32f6489a3af84ff9f13d196571d852
allanzaparoli <allanzaparoli@gmail.com>
2022-02-18 13:13:25 -0300

    atualização requisitos

--- script.js
+++ script.js
@@ -1,72 +1,64 @@
+/* eslint-disable no-param-reassign */
+const takePalete = document.querySelector('#pixel-board');
+const takeColorPalete = document.querySelector('#color-palette');
+const takeColor1 = document.querySelector('#corQuadPalet1');
+const takeColor2 = document.querySelector('#corQuadPalet2');
+const takeColor3 = document.querySelector('#corQuadPalet3');
+const takeColor4 = document.querySelector('#corQuadPalet4');
+let print = 'black';
+const btnCleanPixel = document.createElement('button');
+const cleanP = document.getElementById('clean-pixel');
 
-const takePalete = document.querySelector("#pixel-board");
-const takeColorPalete = document.querySelector("#color-palette");
-let takeColor1 = document.querySelector("#corQuadPalet1");
-let takeColor2 = document.querySelector("#corQuadPalet2");
-let takeColor3 = document.querySelector("#corQuadPalet3");
-let takeColor4 = document.querySelector("#corQuadPalet4");
-let print ="corQuadPalet1";
-let printClean = "white";
-let btnCleanPixel = document.createElement("button");
-const cleanP = document.getElementById("clean-pixel");
-
-function pixels(){
-  for(let index = 1; index <= 25; index +=1){
-  const quadPalete = document.createElement("div");
-  quadPalete.className = "pixel";
-  takePalete.appendChild(quadPalete);
+function pixels() {
+  for (let index = 1; index <= 25; index += 1) {
+    const quadPalete = document.createElement('div');
+    quadPalete.className = 'pixel';
+    takePalete.appendChild(quadPalete);
   }
-};
+}
 pixels();
 
 function removeSelected() {
-  takeColor1.classList.remove("selected");
-  takeColor2.classList.remove("selected");
-  takeColor3.classList.remove("selected");
-  takeColor4.classList.remove("selected");
+  takeColor1.classList.remove('selected');
+  takeColor2.classList.remove('selected');
+  takeColor3.classList.remove('selected');
+  takeColor4.classList.remove('selected');
 }
 
 function takeSelected(event) {
-  let corSelected = document.querySelector(".selected");
-  if(event.target.className !== corSelected) {
+  const corSelected = document.querySelector('.selected');
+  if (event.target.className !== corSelected) {
     removeSelected();
-    event.target.classList.add("selected");
-    print = event.target.id;
+    event.target.classList.add('selected');
+    print = getComputedStyle(event.target).backgroundColor;
   }
 }
-takeColorPalete.addEventListener("click", takeSelected);
-
-
+takeColorPalete.addEventListener('click', takeSelected);
 
 function printPixel(event) {
-  if(event.target.id !== "pixel-board") {
-    event.target.id = print;
+  if (event.target.id !== 'pixel-board') {
+    event.target.style.backgroundColor = print;
     // console.log(pintaCor.style.backgroundColor = "blue");
   // }
   // Outra forma de fazer!!!
   // if(event.target.className === "pixel") {
   //   event.target.style.backgroundColor = "black";
   // }
+  }
 }
-}
-takePalete.addEventListener("click", printPixel);
-
+takePalete.addEventListener('click', printPixel);
 
 function btnClean() {
-  btnCleanPixel.innerText = "Limpar";
-  btnCleanPixel.id = "clear-board";
+  btnCleanPixel.innerText = 'Limpar';
+  btnCleanPixel.id = 'clear-board';
   cleanP.appendChild(btnCleanPixel);
 }
 btnClean();
 
-let painted = document.querySelectorAll(".pixel");
-function cleanPixel(event) {
-  if(btnCleanPixel.style.backgroundColor !== "white") {
-    painted.style.backgroundColor = printClean;
+function cleanPixel() {
+  const quadPalete = document.getElementsByClassName('pixel');
+  for (let index = 0; index < quadPalete.length; index += 1) {
+    quadPalete[index].style.backgroundColor = 'white';
   }
-  // painted.style.backgroundColor = "white";
-  // console.log(painted);
-  // if(event.target.backgroundColor === "pixel") {
-  // }
 }
-btnCleanPixel.addEventListener("click", cleanPixel);
+btnCleanPixel.addEventListener('click', cleanPixel);
